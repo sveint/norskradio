17fb1f1a7fc84b9dee6efa8feddd4f0403d413ff
Svein Tore <sveint@gmail.com>
2017-09-26 23:19:19 +0200

    Improve check_urls script

--- resources/check_urls.py
+++ resources/check_urls.py
@@ -2,18 +2,31 @@ import urllib
 import os
 import json
 
+AAC_MAGIC = [0xff, 0xf1]
+MP3_MAGIC = [0xff, 0xfb]
+ICY_MAGIC = [0x49, 0x43, 0x59]
+FLAC_MAGIC = [0x66, 0x4C, 0x61, 0x43]
+
+
 url_failed = []
 logo_failed = []
+
+def is_stream(data):
+    for test in [AAC_MAGIC, MP3_MAGIC, ICY_MAGIC, FLAC_MAGIC]:
+        if all(a == ord(b) for a, b in zip(test, data)):
+            return True
+
 
 with open('channels.json') as f:
     channels = json.load(f)
 
     for channel in channels:
         passed = False
-        try: 
-            url_ok = urllib.urlopen(channel['url']).read(1024)
+        try:
+            stream = urllib.urlopen(channel['url']).read(2)
+            url_ok = is_stream(stream)
             logo_ok = os.path.exists(os.path.join('logos', channel['logo'] + '.png'))
-            
+
             if not url_ok:
                 url_failed.append(channel)
             if not logo_ok:
@@ -22,7 +35,7 @@ with open('channels.json') as f:
                 passed = True
 
         except Exception:
-            failed.append(channel)
+            url_failed.append(channel)
         print u'{}: {}'.format(channel['name'], passed)
 
 if url_failed or logo_failed:
